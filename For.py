a75926c986f392320b50812450bb3a0c5926db89
carrymec <carrymec@126.com>
2019-04-19 15:53:33 +0800

    test

--- For.py
+++ For.py
@@ -13,4 +13,17 @@ print(arr)
 cop = []
 cop = arr[1:3]
 print(cop)
-print('git test commit')
+cop.clear()
+print(cop)
+num = [1, 5, 3, 9, 8, 7, 1, 3, 5]
+num.sort(reverse=True)
+print(num)
+
+tip = (1, 2, 3, 4, 5)
+print(tip)
+print(type(tip))
+tupe = ('chen', 'hello', 'deng')
+tupe = tupe[:1] + ('dear',) + tupe[1:]
+print(tupe)
+st = 'aaa'
+print(st.capitalize())
